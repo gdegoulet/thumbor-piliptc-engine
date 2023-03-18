53578ff15f41bfd8b7cd1887bcf4ec8da71bb750
Guillaume Degoulet <github@degoulet.net>
2023-03-18 09:56:17 +0100

    change BUILD_DATE format

--- docker/build.sh
+++ docker/build.sh
@@ -3,8 +3,8 @@ cd $(dirname $(readlink -f $0))
 test ! -f ../thumbor_piliptc_engine/engine.py && exit 1
 THUMBOR_VERSION=$(fgrep __version__ ../thumbor_piliptc_engine/engine.py |cut -d "'" -f2 | cut -d '.' -f1-3)
 echo "building $THUMBOR_VERSION"
-docker build -t gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}-sse4 --build-arg THUMBOR_VERSION=$THUMBOR_VERSION --build-arg BUILD_DATE=$(date +%Y%m%d%H%M) --build-arg SIMD_LEVEL=sse4 .
-docker build -t gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}      --build-arg THUMBOR_VERSION=$THUMBOR_VERSION --build-arg BUILD_DATE=$(date +%Y%m%d%H%M) .
+docker build -t gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}-sse4 --build-arg THUMBOR_VERSION=$THUMBOR_VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg SIMD_LEVEL=sse4 .
+docker build -t gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}      --build-arg THUMBOR_VERSION=$THUMBOR_VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') .
 docker tag gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION} gdegoulet/thumbor_piliptc_engine:latest
 
 echo gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}
