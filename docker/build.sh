aa420dbdb0e0e194449392c50745c3a4ae826c73
Guillaume Degoulet <github@degoulet.net>
2023-03-20 09:27:52 +0100

    no image shortname

--- docker/build.sh
+++ docker/build.sh
@@ -3,9 +3,9 @@ cd $(dirname $(readlink -f $0))
 test ! -f ../thumbor_piliptc_engine/engine.py && exit 1
 THUMBOR_VERSION=$(fgrep __version__ ../thumbor_piliptc_engine/engine.py |cut -d "'" -f2 | cut -d '.' -f1-3)
 echo "building $THUMBOR_VERSION"
-docker build -t docker.io/gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}-avx2 --build-arg THUMBOR_VERSION=$THUMBOR_VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg SIMD_LEVEL=avx2 .
-docker build -t docker.io/gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}-sse4 --build-arg THUMBOR_VERSION=$THUMBOR_VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg SIMD_LEVEL=sse4 .
-docker build -t docker.io/gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}      --build-arg THUMBOR_VERSION=$THUMBOR_VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') .
+docker build --no-cache --pull -t docker.io/gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}-avx2 --build-arg THUMBOR_VERSION=$THUMBOR_VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg SIMD_LEVEL=avx2 .
+docker build --no-cache --pull -t docker.io/gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}-sse4 --build-arg THUMBOR_VERSION=$THUMBOR_VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg SIMD_LEVEL=sse4 .
+docker build --no-cache --pull -t docker.io/gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}      --build-arg THUMBOR_VERSION=$THUMBOR_VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') .
 docker tag docker.io/gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION} docker.io/gdegoulet/thumbor_piliptc_engine:latest
 
 echo gdegoulet/thumbor_piliptc_engine:${THUMBOR_VERSION}
